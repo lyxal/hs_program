cf347a5df4ee740caab160060472b1199f343b46
JonoCode9374 <36217120+JonoCode9374@users.noreply.github.com>
2018-05-01 18:56:31 +1000

    Updated screens.py for future user friendlyness

--- screens.py
+++ screens.py
@@ -10,6 +10,7 @@ class Screen:
         self.widgets = list()
         self.name = p_name
         self.window = p_window
+        self.hidden = True
 
     def add_item(self, p_widget, p_row, p_column):
         """
@@ -19,13 +20,47 @@ class Screen:
 
         Returns: Nothing
         """
-        self.widgets.append((p_widget, p_row, p_column))
+        self.widgets.append([p_widget, p_row, p_column])
         
     def show(self):
         for item in self.widgets:
             item[0].grid(row=item[1], column=item[2])
         self.window.title(self.name)
+        self.hidden = False
             
     def hide(self):
         for item in self.widgets:
             item[0].grid_forget()
+        self.hidden = True
+
+    def __iter__(self):
+        return iter(self.widgets)
+
+    def update_widget(self, count, new_x):
+        self.widgets[count][1] = new_x
+
+class ScreenXY(Screen):
+    def add_item(self, p_widget, p_x, p_y):
+        '''
+        Takes: Self, a tkinter widget, and the x/y co-ordinates
+
+        Does: Adds the item to the list of widgets to show
+
+        Returns: Nothing
+        '''
+
+        
+        self.widgets.append([p_widget, p_x, p_y, p_x, p_y])
+
+    def show(self):
+        for item in self.widgets:
+            item[0].place(x=item[1], y=item[2])
+
+        self.window.title(self.name)
+        self.hidden = False
+
+    def hide(self):
+        for item in self.widgets:
+            item[0].place_forget()
+        self.hidden = True
+
